19e017eb5fa9c36aac9544cc57589ad68cd1461f
Namit <namitm@sodelsolutions.com>
2016-07-18 12:57:28 +0530

    YARO-1 : selected tweet on banner

--- banner/load_tweet.php
+++ banner/load_tweet.php
@@ -5,7 +5,7 @@ if(isset($_POST) && isset($_POST['next'])) {
 
 		$next = $_POST['next'];
 		try{
-			$stmt = $conn->prepare("SELECT concat('@',user_screen_name) user_screen_name, profile_photo, question, ans, reply_img FROM yaro_tweets_data order by id desc LIMIT :incrementValue,1"); 
+			$stmt = $conn->prepare("SELECT concat('@',user_screen_name) user_screen_name, profile_photo, question, ans, reply_img, show_on_banner FROM yaro_tweets_data where show_on_banner=1  order by id desc LIMIT :incrementValue,1"); 
 			$stmt->bindValue(':incrementValue', intval($next+1), PDO::PARAM_INT);
 			$stmt->execute();
 			$result = $stmt->fetch(PDO::FETCH_ASSOC);
@@ -13,13 +13,15 @@ if(isset($_POST) && isset($_POST['next'])) {
 			if(!$result) {
 
 			$next = 0;
-			$stmt = $conn->prepare("SELECT concat('@',user_screen_name) user_screen_name, profile_photo, question, ans, reply_img FROM yaro_tweets_data order by id desc LIMIT :incrementValue,1"); 
-			$stmt->bindValue(':incrementValue', intval($next+1), PDO::PARAM_INT);
+			$stmt = $conn->prepare("SELECT concat('@',user_screen_name) user_screen_name, profile_photo, question, ans, reply_img, show_on_banner FROM yaro_tweets_data where show_on_banner=1 order by id desc LIMIT :incrementValue,1"); 
+			$stmt->bindValue(':incrementValue', intval($next), PDO::PARAM_INT);
 			$stmt->execute();
 			$result = $stmt->fetch(PDO::FETCH_ASSOC);
 
+			$result['nextValue'] = $next;
+			}else{
+				$result['nextValue'] = $next+1;
 			}
-			$result['nextValue'] = $next+1;
 			echo json_encode($result);
 			exit();
 
